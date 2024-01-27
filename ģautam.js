66aa27c79d1f1da93f383d47653bfd91bdd51bf0
Greninjahunter2 <157300870+Greninjahunter2@users.noreply.github.com>
2024-01-27 15:03:26 +0530

    Update and rename ģautam.js to gautam.html

--- ģautam.js
+++
@@ -1,9 +0,0 @@
-<button id="myButton">Click me</button>
-
-   <script>
-     document.getElementById("myButton").addEventListener("click", myFunction);
-
-     function myFunction() {
-       // Your command here
-     }
-   </script>
