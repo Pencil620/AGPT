2c5c768c4f0ec70a61bdda455a9539b6bdbec9b3
Greninjahunter2 <157300870+Greninjahunter2@users.noreply.github.com>
2024-01-27 12:17:29 +0530

    Update ģautam.js
    hi

--- ģautam.js
+++ ģautam.js
@@ -1,1 +1,9 @@
+<button id="myButton">Click me</button>
 
+   <script>
+     document.getElementById("myButton").addEventListener("click", myFunction);
+
+     function myFunction() {
+       // Your command here
+     }
+   </script>
